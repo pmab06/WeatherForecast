ae5866c41215dfcc3e17bd5ffe7c0254a36a1415
pmab06 <navybluue.1@gmail.com>
2024-11-06 13:44:32 -0300

    Estrutura inicial

--- script.js
+++ script.js
@@ -1,37 +1,58 @@
-const apiIbge = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+import api_key from './env.js'
+console.log(api_key)
 
+// Função simples para retornar uma opção em HTML (string)
+function createOption(optionValue, optionText) {
+    return `<option value="${optionValue}">${optionText}</option>`
+}
+
+// Função simples que busca e retorna dados de APIs
 async function getApi(url) {
-    response = await fetch(url);
-    data = await response.json();
+    let response = await fetch(url);
+    let data = await response.json();
     return data;
 }
 
+// Iniciar processamento assim que a página carregar
 window.addEventListener('load', async () => {
-    function createOption(optionValue, optionText) {
-        return `<option value="${optionValue}">${optionText}</option>`
-    }
 
-    const estados = await getApi(apiIbge);
-
+    // elementos HTML
     const selectEstados = document.getElementById('estados');
     const selectCidades = document.getElementById('cidades');
 
+    // dados
+    const estados = await getApi('https://servicodados.ibge.gov.br/api/v1/localidades/estados');
+
+
+
+    // Adiciona opções no select de estados
     estados.forEach(estado => {
         selectEstados.innerHTML += createOption(estado.id, estado.nome)
     });
 
+
+
+    // Adiciona listener de eventos no select estado
     selectEstados.addEventListener('change', async function () {
-
-        const cidades = await getApi(apiIbge + "/" + this.value + '/municipios');
-
-        // selectCidades.style.opacity = '100'
-        selectCidades.style.display = 'block'
-
+        const cidades = await getApi(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${this.value}/municipios`);
+        if (this.value != 'none') {
+            selectCidades.style.display = 'block'
+        }else{
+            selectCidades.style.display = 'none'
+        }
         cidades.forEach(cidade => {
             selectCidades.innerHTML += createOption(cidade.id, cidade.nome)
         });
-        // selectCidades.addEventListener('change', async function () {
-
-        // })
     })
+
+
+
+    // Adiciona listener de eventos no select cidades
+    selectCidades.addEventListener('change', async function () {
+        weatherData = await getApi('')         
+    });
+
+
+
+    console.log
 })
