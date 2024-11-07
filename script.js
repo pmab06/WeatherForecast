afc4aba4b26ef80c5e09af3fee9040a368bb97cf
pmab06 <navybluue.1@gmail.com>
2024-11-07 14:00:40 -0300

    Programa funcional, quase completo. Resta adicionar chave da API no repo público.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 import api_key from './env.js'
-console.log(api_key)
 
 // Função simples para retornar uma opção em HTML (string)
 function createOption(optionValue, optionText) {
@@ -13,16 +12,25 @@ async function getApi(url) {
     return data;
 }
 
+function doisDigitos(n) {
+    return (n < 10 ? '0' : '') + n;
+}
 // Iniciar processamento assim que a página carregar
 window.addEventListener('load', async () => {
 
-    // elementos HTML
+    // Elementos HTML
+    const mainWrapper = document.querySelector('main')
     const selectEstados = document.getElementById('estados');
     const selectCidades = document.getElementById('cidades');
+    const infoClima = document.getElementById('weather-wrapper');
+    const iconeClima = document.getElementById('weatherIcon');
+    const hora = document.getElementById('hora')
+    const data = document.getElementById('data')
 
-    // dados
+    let doisP = true // variável pra piscar o : no relógio 
+
+    // Dados Estados
     const estados = await getApi('https://servicodados.ibge.gov.br/api/v1/localidades/estados');
-
 
 
     // Adiciona opções no select de estados
@@ -30,29 +38,69 @@ window.addEventListener('load', async () => {
         selectEstados.innerHTML += createOption(estado.id, estado.nome)
     });
 
-
+    let clockUpdate = null;
 
     // Adiciona listener de eventos no select estado
     selectEstados.addEventListener('change', async function () {
-        const cidades = await getApi(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${this.value}/municipios`);
-        if (this.value != 'none') {
-            selectCidades.style.display = 'block'
-        }else{
-            selectCidades.style.display = 'none'
+        if (estados) {
+            let estadoSelecionado = estados.find(estado => estado.id === Number(this.value)).nome
+            if (this.value != 'none') {
+                selectCidades.style.display = 'block'
+            } else {
+                selectCidades.style.display = 'none'
+            }
+            //Dados da API
+            const cidades = await getApi(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${this.value}/municipios`);
+            selectCidades.innerHTML = createOption('none', '-- Selecione uma cidade --')
+
+            cidades.forEach(cidade => {
+                selectCidades.innerHTML += createOption(cidade.id, cidade.nome)
+            });
+
+            // Adiciona listener de eventos no select cidades
+            selectCidades.addEventListener('change', async function () {
+                console.log(clockUpdate)
+                let cidadeSelecionada = cidades.find(cidade => cidade.id === Number(this.value)).nome
+                let weatherData = await getApi(`https://api.weatherapi.com/v1/current.json?key=${api_key}&q=${estadoSelecionado}, ${cidadeSelecionada}&aqi=yes&lang=pt`)
+
+                infoClima.style.display = 'flex';
+
+                if (this.value != 'none' && !weatherData.error) {
+                    document.getElementById('resposta').style.display = 'none'
+
+                    let dataLocalEscolhido = new Date(weatherData.location.localtime)
+                    // Ícone do clima
+                    iconeClima.src = weatherData.current.condition.icon.replace('64x64', '128x128')
+                    document.getElementById('temps').style.display = 'flex'
+                    document.getElementById('temperatura').innerHTML = `${weatherData.current.temp_c}°C`
+                    document.getElementById('sensacao').innerHTML = `Sensação de ${weatherData.current.feelslike_c}°C`
+                    document.getElementById('addr-cidade').innerHTML = `${weatherData.location.name}`
+                    document.getElementById('addr-estado').innerHTML = `${weatherData.location.region}, ${weatherData.location.country}`
+                    document.getElementById('mensagem').innerHTML = `${weatherData.current.condition.text}`
+                    if (clockUpdate) {
+                        clearInterval(clockUpdate)
+                        console.log('CLOCK UPDATE APAGADO')
+                    }
+                    clockUpdate = setInterval(() => {
+                        doisP = !doisP
+                        hora.innerHTML = `${doisDigitos(dataLocalEscolhido.getHours())}${doisP ? ' ' : ':'}${doisDigitos(dataLocalEscolhido.getMinutes())}`
+                        data.innerHTML = `${doisDigitos(dataLocalEscolhido.getDate())}/${doisDigitos(dataLocalEscolhido.getMonth() + 1)}/${doisDigitos(dataLocalEscolhido.getFullYear())}`
+                    }, 1000)
+                    console.log('CLOCKUPDATE CONFIGURADO COM ', dataLocalEscolhido, " - ", clockUpdate)
+                    // Muda a cor do wrapper dependendo da hora
+                    if (dataLocalEscolhido.getHours() > 17) {
+                        mainWrapper.classList.add('noite')
+                    } else if (dataLocalEscolhido.getHours() > 15) {
+                        mainWrapper.classList.add('tarde')
+                    } else {
+                        mainWrapper.classList.remove('tarde', 'noite')
+                    }
+                } else {
+                    document.getElementById('resposta').style.display = 'flex'
+                    document.getElementById('temps').style.display = 'none'
+                }
+            });
         }
-        cidades.forEach(cidade => {
-            selectCidades.innerHTML += createOption(cidade.id, cidade.nome)
-        });
     })
 
-
-
-    // Adiciona listener de eventos no select cidades
-    selectCidades.addEventListener('change', async function () {
-        weatherData = await getApi('')         
-    });
-
-
-
-    console.log
 })
